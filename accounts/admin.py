ee96b43b2981cb4bf1047c78e603b88094be1714
Tapan Kumar <tapankr7898@gmail.com>
2022-10-17 08:55:22 +0530

    this is my first commit

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+#for making password as read only and what should be displayed on admin page like first name last NAME
+class AccountAdmin(UserAdmin):
+    list_display=('email','first_name','last_name','username','last_login','date_joined','is_active')
+    list_display_links=('email','first_name','last_name')
+    readonly_fields=('last_login','date_joined')
+    odering=('-date_joined')
+
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+
+
+admin.site.register(Account,AccountAdmin)
